be20b449626b8195399296c1aec331f1fb0f82d6
quijano <safiro.quijano@nokia.com>
2020-04-10 19:18:33 +0800

    sample on how to update document in mongodb

--- mongodb.js
+++ mongodb.js
@@ -13,23 +13,17 @@ MongoClient.connect(connectionURL, {useNewUrlParser:true, useUnifiedTopology:tru
     //fetch db via client
     const db = client.db(databaseName);
 
-    // db.collection('users').findOne({
-    //     name: "Bonna"
-    // },(error,response)=>{
-    //     if(error) return console.log('did not find query');
-    //     console.log(response);
-    // });
-
-    // db.collection('users').findOne({
-    //     _id: new ObjectID("5e8ed4ba0a129043656ac70c")
-    // },(error,response)=>{
-    //     if (error) return console.log(error);
-    //     console.log(response);
-    // });
-
-    const results = db.collection('users').find({
-        age: 28
+    const resultUpdate = db.collection('users').updateOne({
+        _id: new ObjectID("5e8f442b3e2acffcfcb82802")
+    },{
+        //for update operations, an update operator ($set for example) is needed
+        $set:{name:"Safiro"}
     });
-    results.forEach(curr=>console.log(curr));
-    
+
+    resultUpdate.then((data)=>{
+        console.log(`data ${data}`);
+    }).catch((error)=>{
+        console.log(`error`);
+    });
+
 });
